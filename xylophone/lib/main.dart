6335a019a9094877f78dc263e2a8ec1870bbfc03
장승호 <hoosong0235@kaist.ac.kr>
2023-01-13 17:52:07 +0900

    learn-flutter

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
+}
+
+Expanded block(int num, Color color) {
+  return Expanded(
+    child: TextButton(
+      onPressed: () {
+        final player = AudioPlayer();
+        player.setAsset('assets/note$num.wav');
+        player.play();
+      },
+      child: Container(
+        color: color,
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,13 +29,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          backgroundColor: Colors.redAccent,
           appBar: AppBar(
-            backgroundColor: Colors.red,
-            title: Text('Xylophone'),
             centerTitle: true,
+            title: Text('Xylophone'),
           ),
-          body: Center(),
+          body: Center(
+            child: Column(
+              children: [
+                block(1, Colors.red),
+                block(2, Colors.orange),
+                block(3, Colors.yellow),
+                block(4, Colors.green),
+                block(5, Colors.blue),
+                block(6, Colors.indigo),
+                block(7, Colors.purple),
+              ],
+            ),
+          ),
         ),
       ),
     );
