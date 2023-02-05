12e99fdc8d449d74e85c78a83852a28a10acc190
장승호 <hoosong0235@kaist.ac.kr>
2023-02-06 08:15:34 +0900

    final commit

--- todoey/lib/main.dart
+++ todoey/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todoey/screen.dart';
 import 'package:todoey/todoprovider.dart';
 import 'package:provider/provider.dart';
-
+  
 void main() {
   runApp(const MyApp());
 }
